97882feb9df79dab5849f6d9c54b0d37d9a92bb1
Sushil Gupta <sushil.mriirs@gmail.com>
2025-10-30 15:16:59 +0530

    Update Operation

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,9 @@ public class Main {
         String url = "jdbc:mysql://127.0.0.1:3306/my_database"  ;
         String username = "root";
         String password = "sg@121";
-        String query = "DELETE FROM employee where id = 3;";
+        String query = "Update employee\n" +
+                "SET job_title = 'Data Analyst',salary = 120000\n" +
+                "Where id = 2";
         try{
          Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("Drivers loaded successfully!! ");
@@ -17,9 +19,9 @@ public class Main {
             Statement stmt = con.createStatement();
             int rowsAffected = stmt.executeUpdate(query);
             if(rowsAffected > 0){
-                System.out.println("Delection successful " + rowsAffected + "row(s) affected");
+                System.out.println("Updation successful " + rowsAffected + "row(s) affected");
             }else{
-                System.out.println("Delection failed !!");
+                System.out.println("Updation failed !!");
             }
 
             stmt.close();
